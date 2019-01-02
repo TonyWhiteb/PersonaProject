9030d5213d75e79fe9d9b6fdebe21b90b813f5c8
TonyWhiteb <tonybai2016@gmail.com>
2019-01-02 15:29:41 -0600

    CreateModels

--- persona/uploadimage/models.py
+++ persona/uploadimage/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+
 
 # Create your models here.
+from django.db import models # import the Django models namespace
+
+
+# Our main model: Uploaded Image
+class UploadedImage(models.Model):
+    image = models.ImageField("Uploaded image") # stores the filename of an uploaded image
