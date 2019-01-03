e465da88e5ccfbf3a4d915373bd9d6fb8a290f5e
TonyWhiteb <tonybai2016@gmail.com>
2019-01-03 15:47:15 -0600

    Update Models

--- persona/uploadimage/models.py
+++ persona/uploadimage/models.py
@@ -1,9 +1,15 @@
 
 
 # Create your models here.
+import uuid
 from django.db import models # import the Django models namespace
 
+def rename_uploaded_filename(instance, filename):
+    extension = filename.split(".")[-1]
+    return "{}.{}".format(uuid.uuid4(), extension)
 
 # Our main model: Uploaded Image
 class UploadedImage(models.Model):
-    image = models.ImageField("Uploaded image") # stores the filename of an uploaded image
+    image = models.ImageField("Uploaded image",upload_to=rename_uploaded_filename) # stores the filename of an uploaded image
+
+
