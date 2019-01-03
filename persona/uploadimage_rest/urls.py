f49052ad14df410a6d5941ec9916ca23efd1697e
TonyWhiteb <tonybai2016@gmail.com>
2019-01-03 12:06:14 -0600

    CreateRESTurls

---
+++ persona/uploadimage_rest/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from django.urls import path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    # url(r'^', include(router.urls)),
+    path('', include(router.urls)),
+]
