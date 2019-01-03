46e11be134c1fe1ac037ed3d21465a9cb23a78b9
TonyWhiteb <tonybai2016@gmail.com>
2019-01-03 15:36:39 -0600

    API Register

--- persona/uploadimage_rest/urls.py
+++ persona/uploadimage_rest/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from django.urls import path
 from rest_framework import routers
+from uploadimage_rest.viewsets import UploadedImagesViewSet
 
 router = routers.DefaultRouter()
-
+router.register('images', UploadedImagesViewSet, 'images')
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
