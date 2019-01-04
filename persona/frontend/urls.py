17be97817e4c191c0e0043bfed4985cb97514e85
TonyWhiteb <tonybai2016@gmail.com>
2019-01-04 10:18:41 -0600

    CreateFrontend

---
+++ persona/frontend/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import path
+from django.views.generic.base import RedirectView
+
+# Wire up our API using automatic URL routing.
+urlpatterns = [
+    path('', RedirectView.as_view(url='persona/templates/base.html', permanent=False), name='index')
+]
